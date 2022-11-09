1215d484dcf55577d736e19f0ae16087ae292130
Tobias Nordahl Kristensen <tobiasnordahl@gmail.com>
2022-11-09 16:59:17 +0100

    Readding copyright

--- src/PinDefinitions.h
+++ src/PinDefinitions.h
@@ -1,3 +1,31 @@
+/*
+ *  PinDefinitionsAndMore.h
+ *
+ *  Contains pin definitions for IRremote examples for various platforms
+ *  as well as definitions for feedback LED and tone() and includes
+ *
+ *  Copyright (C) 2021-2022  Armin Joachimsmeyer
+ *  armin.joachimsmeyer@gmail.com
+ *
+ *  This file is part of IRremote https://github.com/Arduino-IRremote/Arduino-IRremote.
+ *
+ *  Arduino-IRremote is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/gpl.html>.
+ *
+ *  Stripped down by Tobias Nordahl
+ */
+
+
 #if defined(ESP8266)
 #define FEEDBACK_LED_IS_ACTIVE_LOW // The LED on my board (D4) is active LOW
 #define IR_RECEIVE_PIN          5 // D1  = GPIO 5
